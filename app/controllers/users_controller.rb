c61c0b4eb3443578326f5debc96b5a0a44037f97
Jhonatan I <jhonatan-nascimento94@hotmail.com>
2019-11-30 01:46:59 -0300

    CRUD users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,54 +1,64 @@
 class UsersController < ApplicationController
   protect_from_forgery with: :null_session
-  before_action :authorize_request, except: :create
-  before_action :find_user, except: %i[create index]
+  # before_action :authorize_request, except: :create
+  before_action :find_user, except: %i[create index new]
 
 
   # GET /users
   def index
     @users = User.all
-    render json: @users, status: :ok
+    # render json: @users, status: :ok
   end
 
   # GET /users/{username}
   def show
-    render json: @user, status: :ok
+    # render json: @user, status: :ok
+  end
+
+  def new
+    @user = User.new
   end
 
   # POST /users
   def create
     @user = User.new(user_params)
     if @user.save
-      render json: @user, status: :created
+      flash[:alert] = "sucesso!"
+      redirect_to root_path
     else
-      render json: { errors: @user.errors.full_messages },
-             status: :unprocessable_entity
+      flash[:alert] = "Erro ao criar o usuário!"
+      render :new
     end
   end
 
-  # PUT /users/{username}
+  def edit
+    #
+  end
+
   def update
-    unless @user.update(user_params)
-      render json: { errors: @user.errors.full_messages },
-             status: :unprocessable_entity
+    if @user.update(user_params)
+      flash[:alert] = "sucesso!"
+      redirect_to root_path
+    else
+      flash[:alert] = "Erro ao atualizar usuário!"
+      render :edit
     end
   end
 
   # DELETE /users/{username}
   def destroy
     @user.destroy
+    redirect_to root_path
   end
 
   private
 
   def find_user
-    @user = User.find_by_username!(params[:_username])
-  rescue ActiveRecord::RecordNotFound
-    render json: { errors: 'User not found' }, status: :not_found
+    @user = User.find(params[:id])
   end
 
   def user_params
-    params.permit(
+    params.require(:user).permit(
         :name, :username, :email, :password, :password_confirmation
     )
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :users, param: :_username
+  root to: 'users#index'
+  resources :users
   post '/auth/login', to: 'authentication#login'
   get '/*a', to: 'application#not_found'
 
